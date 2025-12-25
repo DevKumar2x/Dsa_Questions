d17f5a1b4f1c1f9064aa9ab47f0f7218b72242d8
DevKumar2x <dev365952@gmail.com>
2025-12-25 16:46:13 +0530

    add Q3.

--- Ques.py
+++ Ques.py
@@ -20,3 +20,95 @@ import calendar
 yy = 2025
 mm = 8
 print(calendar.month(yy,mm))
+
+
+
+
+# Q3. Create a student grading system using Python that has the following functionalities:
+# 1. Entering the Grades of a student
+# 2. Removing a student from the system
+# 3. Calculating the average grades of students
+# The user should be able to select whether he/she wants to remove a student, enter grades for a
+# student or find the average grades.
+# Also add login system to the Grade Central -- only admins can access Grade Central.
+
+ex_dict = {'sam' : [43, 65]}          
+admins = {
+    'Dev' : 'pass123',
+    'user1' : 'pass1'             
+}
+
+def Enter_grade():
+    name = input("Student name : ")
+    grade = int((input("Grade : ")))
+    if name in ex_dict :
+        print("Adding Grade...")
+        ex_dict[name].append(grade)
+    else:
+        print("Adding new student data to the Grade Central...")
+        ex_dict[name] = [grade]
+    print(ex_dict , '\n') 
+
+def Remove_grade():
+    name = input("Which Student to remove : ")
+    print("Removing Student...")
+    if name in ex_dict :
+        del ex_dict[name]
+        print(ex_dict)
+    else :
+        print("Student does not exits.")
+    
+def Average():
+    name = input("Which Student average you want to calculate : ")
+    avg = 0
+    cnt = 0
+    if name in ex_dict:
+        list = ex_dict[name]
+        for i in list :
+            cnt += 1
+            avg = avg + i
+        avg = avg / cnt
+        print("Average is" , avg , '\n')
+    else:
+        print("Student not exist.")
+    
+def main():
+    run = False
+    username = input("Enter username : ")
+    password = input("Enter password : ")
+    
+    if username in admins :
+        if admins[username] == password:
+            run = True
+        else:
+            print("Wrong password. Please try again later.")
+    else:
+        print("Wrong username. Please try again later.")
+    
+    while(run):
+        print("\nWelcome", username ,"to Grade Central\n")
+        print("[1] Enter Grade")
+        print("[2] Remove Student")
+        print("[3] Students Average Grade")
+        print("[4] Exit")
+        
+        choice = input("What would you like to do today? [Enter a number] : ")
+        
+        if(choice == "1"):
+            Enter_grade()
+        
+        elif(choice == "2"):
+            Remove_grade()
+        elif(choice == "3"):
+            Average()
+        elif(choice == "4") :
+            print("Exit Grade Central. Visit again.")
+            break
+        else :
+            print("Invaid input.")
+
+if __name__ == '__main__':
+    main()
+    
+
+#Q4.          ...(To be added next)...
